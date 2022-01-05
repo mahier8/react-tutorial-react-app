ba5224190d2a56407129856451c046d95a1b3eff
Mahier <yonhubby@Mahiers-MacBook-Pro.local>
2022-01-05 14:59:56 +0900

    tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React, {Component} from 'react'
-import Table from './Table'
-import "./app.css"
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
+import "./app.css";
 
 class App extends Component {
-  render() {
-    const characters = [
-      {
-        name: "Angel",
-        job: "professor",
-      },
-      {
-        name: "Mahier",
-        job: "life coach"
-      },
-      {
-        name: "Alexis",
-        job: "developer",
-      },
-      {
-        name: "Marie",
-        job: "coding teacher",
-      },
-    ]
+    state = {
+        characters: []
+    };
 
-    return (
-      <div className="container">
-        <Table characterData={characters} />
-      </div>
-    )
-  }
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render() {
+        const { characters } = this.state;
+        
+        return (
+            <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
